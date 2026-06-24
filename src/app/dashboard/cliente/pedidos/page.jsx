dce225ad1eeffea713ef99590ac8546f0c8f76fc
david <jdchavezr917@gmail.com>
2026-06-24 09:17:28 -0600

    prueba de pull

--- src/app/dashboard/cliente/pedidos/page.jsx
+++ src/app/dashboard/cliente/pedidos/page.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { NavbarCliente } from '@/components/layout/NavbarCliente'  // ← Cambiar a NavbarVendedor
+import { NavbarCliente } from '@/components/layout/NavbarCliente'  
 
 const pedidosCliente = () => {
     return (
         <div>
-            <NavbarCliente />  {/* ← Usar NavbarCliente */}
+            <NavbarCliente />  
             <div className='min-h-screen flex items-center justify-center bg-[#f5f5f0]'>
                 <h1 className='text-black'>Mis Pedidos</h1>
             </div>
