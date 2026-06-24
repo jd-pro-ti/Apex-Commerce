79f9384a93e180454a1244c819b9c6d622f5686e
Diego Arellano <diegoarellano521@gmail.com>
2026-06-24 08:15:57 -0600

    prueba de pull

--- src/app/page.js
+++ src/app/page.js
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            prueba
           </a>
         </div>
       </main>
